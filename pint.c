e3be5609ffb67417768f5eb884c01e727087fe2e
yetzabeth <yetzaher@gmail.com>
2023-08-31 08:48:58 -0700

    pint operation correction

--- pint.c
+++ pint.c
@@ -11,7 +11,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *head = *stack;
 
-	if (var.stack_len == 0)
+	if (head == NULL)
 	{
 		fprintf(stdout, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
